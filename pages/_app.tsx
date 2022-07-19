d9a78ea5564ca3670d1a2bcaa23a02523b6b574a
leomet07 <leomet07@gmail.com>
2022-07-19 13:41:47 -0400

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />;
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
