d680444e4c417ef8328faef59aa62231f0e34786
leomet07 <leomet07@gmail.com>
2022-08-02 09:06:52 -0400

    Add umami analytics

--- README.md
+++ README.md
@@ -15,3 +15,6 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ## About
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+For analytics, we use [Umami](https://umami.is/).
+Why? Because Google Analytics should be (and in some places) is illegal. 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<link rel="icon" href="/favicon.ico" />;
+				<link rel="icon" href="/favicon.ico" />
+				<script
+					async
+					defer
+					data-website-id="532b1bdf-a105-4c35-a9aa-c5c57ab6b3bf"
+					src="https://umami-fork-alpha.vercel.app/umami.js"
+				></script>
 			</Head>
 			<div id="page">
 				<Component {...pageProps} />
