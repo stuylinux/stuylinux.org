930547f73b7ac5dd429f3fcc941d02c149e4e121
leomet07 <leomet07@gmail.com>
2023-09-05 10:09:22 -0400

    Update meta description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,5 +2,5 @@ module.exports = {
 	title: "Stuy Linux",
 	url: "https://stuylinux.org/",
 	language: "en",
-	description: "Stuyvesant Linux Club description",
+	description: "Stuy Linux is Stuyvesant's only initiative to promote the use of Free/Libre Open Source Software (FLOSS) programs and the GNU/Linux projects.",
 };
